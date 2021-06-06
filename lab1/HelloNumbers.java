2bc70dd8ac1db58f37e631376edf1d8a3a0e0a62
Anderson <157791105@qq.com>
2021-06-06 08:13:52 +0700

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,15 @@
+public class HelloNumbers{
+    public static void main(String[] args){
+    	int x = 0;
+    	int sum = 0;
+    	while(x < 10){
+	    sum = sum + x;
+	    System.out.print(sum + " ");
+	    x = x + 1;
+	}
+
+	System.out.print(x + "10");
+	System.out.print(5 + 10);
+	System.out.println(5);
+    }
+}
